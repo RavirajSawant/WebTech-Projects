7b25e25edf6fe138221642f331d8cd7141b02835
RavirajSawant <ravirajsawant.sr@gmail.com>
2022-02-05 21:56:50 +0530

    My Projects

---
+++ Mini Project/script.js
@@ -0,0 +1,10 @@
+function task()
+{
+    var element=document.getElementById('x');
+    element.style.fontSize="50px";
+    }
+function task1()
+{
+    var element=document.getElementById('x');
+    element.style.fontSize="1px";
+}
